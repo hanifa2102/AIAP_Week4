3d20404b3b34e31ba546c947a4e3c1753a5d7746
Mohamed Hanifa <t0916985@kelaberetiv.sg>
2018-12-04 21:55:39 +0800

    Day One Commit

--- src/load_img.py
+++ src/load_img.py
@@ -13,11 +13,19 @@ import tarfile,urllib,os
 from sklearn.model_selection import cross_val_score
 from sklearn.neighbors import NearestNeighbors
 
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+    
+    
+
 def unpickle(file):
  '''Load byte data from file'''
  with open(file, 'rb') as f:
-  data = pickle.load(f, encoding='latin-1')
-  return data
+     data = pickle.load(f, encoding='latin-1')
+     return data
 
 
 def download_data():
@@ -55,13 +63,15 @@ def load_unpickledata():
     return train_data,train_labels,test_data,test_labels
 
 
-def visualisedata(train_data,test_data,i):
-    train_data = train_data.reshape((len(train_data), 3, 32, 32))
-    train_data = np.rollaxis(train_data, 1, 4)
-#    test_data = test_data.reshape((len(test_data), 3, 32, 32))
-#    test_data = np.rollaxis(test_data, 1, 4)
+def visualisedata(data,i):
+#    train_data = train_data.reshape((len(train_data), 3, 32, 32))
+#    train_data = np.rollaxis(train_data, 1, 4)
+##    test_data = test_data.reshape((len(test_data), 3, 32, 32))
+##    test_data = np.rollaxis(test_data, 1, 4)
     
-    plt.imshow(train_data[i])
+    data = data.reshape((len(data), 3, 32, 32))
+    
+    plt.imshow(data[i])
     plt.show()
     
     
@@ -75,14 +85,14 @@ test_labels=np.array(test_labels).reshape(-1,1)
 
 
 
-train_data=train_data[0:10000]
-train_labels=train_labels[0:10000]
+train_data=train_data[0:300]
+train_labels=train_labels[0:300]
 
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.pipeline import make_pipeline
 knn_pipeline = make_pipeline(KNeighborsClassifier(n_neighbors=10,p=1))
 ##clf = svm.SVC(kernel='linear', C=1)
-x=cross_val_score(knn, train_data, train_labels.ravel(), cv=5)
+x=cross_val_score(knn_pipeline, train_data, train_labels.ravel(), cv=5,verbose=1,n_jobs=-1)
 
 
 
